f2539a13a43251706ff674e64019cef550c9653d
hippieperm <leess0133@gmail.com>
2025-08-20 03:28:24 +0900

    LableBox 위젯 수정: 텍스트 표시를 '선택됨'으로 변경하고, Row 레이아웃으로 구성하여 UI 개선

--- lib/seat_page/widgets/lable_box.dart
+++ lib/seat_page/widgets/lable_box.dart
@@ -5,6 +5,11 @@ class LableBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('LableBox');
+    return Row(
+      children: [
+        Container(),
+        Text('선택됨'),
+      ],
+    );
   }
 }
