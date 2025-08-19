1e7e1aa443971b4c38e7d607be2de58c87a09f0d
hippieperm <leess0133@gmail.com>
2025-08-20 03:08:14 +0900

    홈페이지 UI 개선: AppBar에 제목 '좌석 선택' 추가하여 사용자 경험 향상

--- lib/seat_page.dart
+++ lib/seat_page.dart
@@ -6,7 +6,9 @@ class SeatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('좌석 선택'),
+      ),
       body: Center(
         child: Text('SeatPage'),
       ),
