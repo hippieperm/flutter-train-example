4c8b4a967bf733c026f60cc0927ed4d6b1707dcb
hippieperm <leess0133@gmail.com>
2025-08-20 03:36:08 +0900

    LableBox 위젯 수정: 중앙 정렬 추가 및 선택 상태를 나타내는 두 개의 텍스트와 색상 박스 구성으로 UI 개선

--- lib/seat_page/widgets/lable_box.dart
+++ lib/seat_page/widgets/lable_box.dart
@@ -6,9 +6,28 @@ class LableBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(),
-        Text('선택됨'),
+        lable('선택됨', Colors.purple),
+        SizedBox(width: 20),
+        lable('선택안됨', Colors.grey),
+      ],
+    );
+  }
+
+  Row lable(String text, Color color) {
+    return Row(
+      children: [
+        Container(
+          width: 24,
+          height: 24,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+        SizedBox(width: 4),
+        Text(text),
       ],
     );
   }
