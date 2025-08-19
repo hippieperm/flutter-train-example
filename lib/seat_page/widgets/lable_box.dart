9734f4d66f16409e8d2855a1507eea9069edb339
hippieperm <leess0133@gmail.com>
2025-08-20 03:11:09 +0900

    좌석 선택 페이지 위젯 추가: LableBox 클래스를 생성하여 텍스트 표시 기능 구현

---
+++ lib/seat_page/widgets/lable_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LableBox extends StatelessWidget {
+  const LableBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('LableBox');
+  }
+}
