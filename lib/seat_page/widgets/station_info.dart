e95dafd4375c9a793af069128e2eede3bf0f9689
hippieperm <leess0133@gmail.com>
2025-08-20 03:26:57 +0900

    StationInfo 위젯 수정: 텍스트 스타일과 레이아웃을 개선하여 출발역과 도착역 정보를 시각적으로 표시하도록 변경

--- lib/seat_page/widgets/station_info.dart
+++ lib/seat_page/widgets/station_info.dart
@@ -5,6 +5,34 @@ class StationInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('StationInfo');
+    return DefaultTextStyle(
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.purple,
+        fontSize: 30,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Center(
+              child: Text(
+                '수서',
+              ),
+            ),
+          ),
+          Icon(
+            Icons.arrow_circle_right_outlined,
+            size: 30,
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                '부산',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
