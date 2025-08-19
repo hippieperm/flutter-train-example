7938fad96d4d690efd0a7955f2852fa40b2af8a8
hippieperm <leess0133@gmail.com>
2025-08-20 03:10:13 +0900

    좌석 선택 페이지 추가: SeatPage 클래스를 새로 생성하여 좌석 선택 UI를 구현

---
+++ lib/seat_page/seat_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SeatPage extends StatelessWidget {
+  const SeatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('좌석 선택'),
+      ),
+      body: Center(
+        child: Text('SeatPage'),
+      ),
+    );
+  }
+}
