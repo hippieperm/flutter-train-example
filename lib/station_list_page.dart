8b27e2303c64c77d2dd367b159ac7d9943aada8c
hippieperm <leess0133@gmail.com>
2025-08-19 16:52:46 +0900

    홈페이지 UI 개선: 출발역 목록을 추가하고 AppBar에 제목을 설정하여 사용자 경험 향상

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -5,10 +5,34 @@ class StationListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> stations = [
+      "수서",
+      "동탄",
+      "평택지제",
+      "천안아산",
+      "오송",
+      "대전",
+      "김천구미",
+      "동대구",
+      "경주",
+      "울산",
+      "부산"
+    ];
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('StationListPage'),
+      appBar: AppBar(
+        title: Text('출발역'),
+      ),
+      body: ListView.builder(
+        itemCount: stations.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(
+            stations[index],
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          );
+        },
       ),
     );
   }
