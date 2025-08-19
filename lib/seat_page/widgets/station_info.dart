660aa3bdfa21aa7f56a16623972c40050a5d0298
hippieperm <leess0133@gmail.com>
2025-08-20 03:10:17 +0900

    좌석 정보 위젯 추가: StationInfo 클래스를 생성하여 좌석 정보 표시 기능 구현

---
+++ lib/seat_page/widgets/station_info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StationInfo extends StatelessWidget {
+  const StationInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('StationInfo');
+  }
+}
