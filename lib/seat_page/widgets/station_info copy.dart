195273f38902af1d6c0dd96cabe1956a3dccacd4
hippieperm <leess0133@gmail.com>
2025-08-20 03:12:33 +0900

    좌석 정보 표시를 위한 SeatListView 위젯 추가: StationInfo 클래스를 기반으로 구현

---
+++ lib/seat_page/widgets/station_info copy.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SeatListView extends StatelessWidget {
+  const SeatListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('StationInfo');
+  }
+}
