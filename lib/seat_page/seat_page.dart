7aabe8b207f99e39e33018d6049f1043a877184c
hippieperm <leess0133@gmail.com>
2025-08-20 06:02:50 +0900

    SeatPage 위젯을 StatelessWidget에서 StatefulWidget으로 변경하고, 좌석 선택 기능을 위한 상태 관리 로직을 추가하여 사용자 경험을 개선함

--- lib/seat_page/seat_page.dart
+++ lib/seat_page/seat_page.dart
@@ -3,7 +3,7 @@ import 'package:train/seat_page/widgets/lable_box.dart';
 import 'package:train/seat_page/widgets/seat_list_view.dart';
 import 'package:train/seat_page/widgets/station_info.dart';
 
-class SeatPage extends StatelessWidget {
+class SeatPage extends StatefulWidget {
   const SeatPage({
     super.key,
     required this.startStation,
@@ -12,6 +12,21 @@ class SeatPage extends StatelessWidget {
 
   final String startStation;
   final String endStation;
+
+  @override
+  State<SeatPage> createState() => _SeatPageState();
+}
+
+class _SeatPageState extends State<SeatPage> {
+  int? selectedColNum;
+  int? selectedRowNum;
+
+  void onSelectedSeat(int rowNum, int colNum) {
+    setState(() {
+      selectedColNum = colNum;
+      selectedRowNum = rowNum;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +40,15 @@ class SeatPage extends StatelessWidget {
             child: Column(
               children: [
                 StationInfo(
-                  startStation: startStation,
-                  endStation: endStation,
+                  startStation: widget.startStation,
+                  endStation: widget.endStation,
                 ),
                 LableBox(),
-                SeatListView(),
+                SeatListView(
+                  selectedColNum: selectedColNum,
+                  selectedRowNum: selectedRowNum,
+                  onSelected: onSelectedSeat,
+                ),
                 SizedBox(
                   width: double.infinity,
                   height: 50,
